c4c203066d5e0505c8fb46c2d42cff4df106b7e1
Prathamesh B <55992548+Prathamesh-B@users.noreply.github.com>
2024-06-19 15:10:07 +0530

    Update Q1482

--- 1482. Minimum Number of Days to Make m Bouquets.java
+++ 1482. Minimum Number of Days to Make m Bouquets.java
@@ -1,24 +1,45 @@
 class Solution {
-    public int minDays(int[] A, int m, int k) {
-        int n = A.length, left = 1, right = (int) 1e9;
-        if (m * k > n)
+    public int minDays(int[] bloomDay, int m, int k) {
+        if ((long) m * k > bloomDay.length) {
             return -1;
-        while (left < right) {
-            int mid = (left + right) / 2, flow = 0, bouq = 0;
-            for (int j = 0; j < n; ++j) {
-                if (A[j] > mid) {
-                    flow = 0;
-                } else if (++flow >= k) {
-                    bouq++;
-                    flow = 0;
-                }
-            }
-            if (bouq < m) {
-                left = mid + 1;
+        }
+
+        int low = 1, high = (int) 1e9;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (isPossibleBouquets(bloomDay, m, k, mid)) {
+                high = mid;
             } else {
-                right = mid;
+                low = mid + 1;
             }
         }
-        return left;
+        ;
+
+        return low;
     }
+
+    private boolean isPossibleBouquets(int[] bloomDay, int m, int k, int day) {
+        int total = 0;
+
+        for (int i = 0; i < bloomDay.length; i++) {
+            int count = 0;
+            while (i < bloomDay.length && count < k && bloomDay[i] <= day) {
+                count++;
+                i++;
+            }
+
+            if (count == k) {
+                total++;
+                i--;
+            }
+
+            if (total >= m) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
 }
