f0fd0d1aea08d793409cbe386c08592a9e049924
Prathamesh B <55992548+Prathamesh-B@users.noreply.github.com>
2024-08-23 21:06:26 +0530

    Update Q592

--- 592. Fraction Addition and Subtraction.java
+++ 592. Fraction Addition and Subtraction.java
@@ -1,31 +1,19 @@
 class Solution {
     public String fractionAddition(String expression) {
-        int numerator = 0, denominator = 1;
-        
-        Pattern pattern = Pattern.compile("([+-]?\\d+)/(\\d+)");
-        Matcher matcher = pattern.matcher(expression);
-        
-        while (matcher.find()) {
-            int num = Integer.parseInt(matcher.group(1));
-            int den = Integer.parseInt(matcher.group(2));
-            
-            numerator = numerator * den + num * denominator;
-            denominator *= den;
-            
-            int gcdVal = gcd(Math.abs(numerator), denominator);
-            numerator /= gcdVal;
-            denominator /= gcdVal;
+        Scanner sc = new Scanner(expression).useDelimiter("/|(?=[-+])");
+        int A = 0, B = 1;
+        while (sc.hasNext()) {
+            int a = sc.nextInt(), b = sc.nextInt();
+            A = A * b + a * B;
+            B *= b;
+            int g = gcd(A, B);
+            A /= g;
+            B /= g;
         }
-        
-        return numerator + "/" + denominator;
+        return A + "/" + B;
     }
-    
+
     private int gcd(int a, int b) {
-        while (b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a;
+        return a != 0 ? gcd(b % a, a) : Math.abs(b);
     }
 }
