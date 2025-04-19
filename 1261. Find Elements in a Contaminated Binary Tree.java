0d3d2bed8c155704099d239c6bd8d2a8f0e98202
Prathamesh B <55992548+Prathamesh-B@users.noreply.github.com>
2025-04-19 23:18:32 +0530

    Update Q1261

--- 1261. Find Elements in a Contaminated Binary Tree.java
+++ 1261. Find Elements in a Contaminated Binary Tree.java
@@ -1,21 +1,20 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 
-
- class FindElements {
+class FindElements {
     BitSet recoveredValues;
 
     public FindElements(TreeNode root) {
@@ -25,7 +24,8 @@
     }
 
     private void recoverTree(TreeNode root) {
-        if (root == null) return;
+        if (root == null)
+            return;
         recoveredValues.set(root.val);
         if (root.left != null) {
             root.left.val = 2 * root.val + 1;
